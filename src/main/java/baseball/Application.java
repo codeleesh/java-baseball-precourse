76ab152fb215b0c611003c81337bd1ff63b5bd23
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-06 02:27:07 +0900

    add implemente main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import baseball.common.Constant;
+import baseball.controller.BaseBallController;
+import baseball.view.BaseBallView;
+import nextstep.utils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        function();
+    }
+
+    public static void function() {
+        BaseBallView baseBallView = new BaseBallView();
+        BaseBallController baseBallController = new BaseBallController(baseBallView);
+
+        int[] randomNum = baseBallController.getGenerateRandomNum();
+        baseBallController.getStart(randomNum);
+        getRetry();
+    }
+
+    public static void getRetry() {
+        String input = Console.readLine();
+        if( Constant.STR_ONE.equals(input) ) {
+            Application.function();
+        }
     }
 }
