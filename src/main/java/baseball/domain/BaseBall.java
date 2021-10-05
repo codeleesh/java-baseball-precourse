18f6f899ccc1301402463657e0f2fded75b8aa0b
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-06 01:48:46 +0900

    Use object BaseBall

---
+++ src/main/java/baseball/domain/BaseBall.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+public class BaseBall {
+
+    private int[] inputNums;
+
+    public int[] getInputNums() {return inputNums;}
+
+    public BaseBall(int[] inputNums) {
+        this.inputNums = inputNums;
+    }
+}
