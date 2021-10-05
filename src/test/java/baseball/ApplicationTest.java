a7a44adea4a26c9718c13751edb7d9f65e672736
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-06 02:28:32 +0900

    test: game end, wrong input, three ball, three strike

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -10,10 +10,31 @@ import org.mockito.MockedStatic;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mockStatic;
 
-public class ApplicationTest extends NSTest {
+class ApplicationTest extends NSTest {
     @BeforeEach
     void beforeEach() {
         super.setUp();
+    }
+
+    @Test
+    void 게임종료() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 2, 8);
+            run("128", "2");
+            verify("3스트라이크", "게임 끝");
+        }
+    }
+
+
+    @Test
+    void 잘못입력한값() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 2, 8);
+            run("45", "128", "2");
+            verify("[ERROR] 3자리를 입력하셔야 합니다.", "3스트라이크", "게임 끝");
+        }
     }
 
     @Test
@@ -28,12 +49,35 @@ public class ApplicationTest extends NSTest {
     }
 
     @Test
+    void 쓰리볼() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 3, 5);
+            running("513");
+            verify("3볼");
+        }
+    }
+
+    @Test
+    void 쓰리스트라이크() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 3, 5);
+            running("135");
+            verify("3스트라이크");
+        }
+    }
+
+    @Test
     void 게임종료_후_재시작() {
         try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
-            mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
                     .thenReturn(7, 1, 3)
                     .thenReturn(5, 8, 9);
-            run("713", "1", "597", "589", "2");
+            run("137", "1", "597", "589", "2");
             verify("3스트라이크", "게임 끝", "1스트라이크 1볼");
         }
     }
