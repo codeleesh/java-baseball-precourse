5bf0d2728923c21086c159f5af6af6b526eb1faf
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-06 02:14:28 +0900

    Use service constant

---
+++ src/main/java/baseball/common/Constant.java
@@ -0,0 +1,18 @@
+package baseball.common;
+
+public final class Constant {
+
+    public static final String STR_EMPTY = "";
+    public static final String STR_ONE = "1";
+    public static final String STR_ERROR = "ERROR";
+    public static final int INT_ZERO = 0;
+    public static final int INT_ONE = 1;
+    public static final int INT_THREE = 3;
+    public static final int INT_NINE = 9;
+    public static final int INT_TEN = 10;
+    public static final String STR_STRIKE = "스트라이크";
+    public static final String STR_BALL = "볼";
+    public static final String STR_NOTHING = "낫싱";
+
+    private Constant(){}
+}
